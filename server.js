5c47a238066ff70ee9c3d2cd58b7be48b2cffef9
Polina Titova <titova_polina@mail.ru>
2022-05-29 02:48:21 +0300

    added bot logic

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const posts = [
   { 
     dateString: '01.01.1970 00:00', 
     message: 'This is test message #1',
-	type: 'text',
+	  type: 'text',
     id: v4(), 
   },
 ];
@@ -69,11 +69,42 @@ wsServer.on('connection', (ws) => {
       const newPost = {
         dateString: request.post.dateString,
         message: request.post.message,
-		type: request.post.type,
+		    type: request.post.type,
         id: v4(),
       };
 
       posts.push(newPost);
+
+      if (newPost.message.match('@chaos: ')) {
+        let botMessage = null;
+        if (newPost.message.match('погода')) {
+          botMessage = 'Погода сейчас: +30, штиль';
+        } else if (newPost.message.match('цой')) {
+          botMessage = 'жив';
+        } else if (newPost.message.match('фаза Луны')) {
+          botMessage = 'Луна растёт';
+        } else if (newPost.message.match('HL3')) {
+          botMessage = 'HL3 всё ещё не вышла';
+        } else if (newPost.message.match('курс')) {
+          botMessage = 'Доллар: 60, Евро: 65';
+        } else {
+          return;
+        }
+
+        const botPost = {
+          dateString: request.post.dateString,
+          message: botMessage,
+          type: request.post.type,
+          id: v4(),
+        };
+        posts.push(botPost);
+        ws.send(JSON.stringify(
+          {
+            event: 'botResponse',
+            allPosts: [botPost],
+          },)
+        );
+      }
     }
 	
 	  if (request.event === 'addFilePost') {
@@ -89,9 +120,22 @@ wsServer.on('connection', (ws) => {
     }
 	
 	  if (request.event === 'getAllPosts') {
+      let postsCount = request.count;
+      let startPostForSend = posts.length - request.count;
+
+      if (request.lastId !== null) {
+        startPostForSend = posts.indexOf(posts.filter(post => post.id === request.lastId)[0]) - request.count;
+      }
+
+      if (startPostForSend + request.count <= 0) return;
+      if (startPostForSend < 0) {
+        postsCount = startPostForSend + request.count;
+        startPostForSend = 0;
+      }
+
       const response = JSON.stringify({
         event: 'getAllPosts',
-        allPosts: posts,
+        allPosts: posts.slice(startPostForSend, startPostForSend + postsCount),
       });
 	  
       ws.send(response);
